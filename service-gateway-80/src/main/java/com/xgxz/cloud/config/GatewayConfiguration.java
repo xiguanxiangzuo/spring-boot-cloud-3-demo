4774428e2c1c80d98b9bd33185a029c5948eccac
xiguanxiangzuo <xiguanxiangzuo@gmail.com>
2024-03-26 22:32:22 +0800

    gateway整合sentinel

---
+++ service-gateway-80/src/main/java/com/xgxz/cloud/config/GatewayConfiguration.java
@@ -0,0 +1,96 @@
+package com.xgxz.cloud.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+
+/**
+ * ClassName: GatewayConfiguration
+ * Package: com.xgxz.cloud.config
+ * Description:
+ *
+ * @Author 习惯向左
+ * @Create 2024/3/26 21:52
+ * @Version 1.0
+ */
+@Configuration
+public class GatewayConfiguration {
+
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        // Register the block exception handler for Spring Cloud Gateway.
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+
+    @PostConstruct
+    public void doInit() {
+
+        initBlockHandler();
+    }
+
+    private void initBlockHandler() {
+
+        Set<GatewayFlowRule> rules = new HashSet<>();
+
+        // 1秒钟请求2次就触发
+        rules.add(new GatewayFlowRule("service-cloud-consumer").setCount(2).setIntervalSec(1));
+
+        GatewayRuleManager.loadRules(rules);
+
+        BlockRequestHandler handler = new BlockRequestHandler() {
+
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+
+                Map<String, String> map = new HashMap<>();
+
+                map.put("errorCode", HttpStatus.TOO_MANY_REQUESTS.getReasonPhrase());
+
+                map.put("errorMessage", "请求太过频繁, 系统忙不过来, 触发限流(sentinel + gateway整合case)");
+
+                return ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromValue(map));
+            }
+        };
+
+        GatewayCallbackManager.setBlockHandler(handler);
+    }
+
+}
